94f911deaa779627e146020ff3c48c2192ec2dda
benjamin <benjamin@stiffcoyote-e9263.denbi.prod>
2026-05-12 13:48:46 +0000

    refactor: mode case statement

--- ci/build.sh
+++ ci/build.sh
@@ -5,34 +5,45 @@ PKGS=(./internal/... ./auth)
 VERSION="$(git describe --tags --always)"
 LDFLAGS="-s -w -X github.com/involucro/involucro/app.version=$VERSION"
 
-if [[ "$MODE" == "unit" ]]; then
+case "$MODE" in
+unit)
     go test -v -short "${PKGS[@]}"
-fi
+    CGO_ENABLED=0 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
+    "./$FILENAME" --version
+    ;;
 
-if [[ "$MODE" == "windows-amd64" ]]; then
+integration)
+    CGO_ENABLED=0 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
+    "./$FILENAME" --version
+    "./$FILENAME" wrap-yourself
+    go test -v "${PKGS[@]}"
+    ;;
+
+windows-amd64)
     go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
     "$GOPATH"/bin/goversioninfo "-product-version=$VERSION"
 
     CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o involucro.exe -ldflags "$LDFLAGS" ./cmd/involucro
     file involucro.exe
+    ;;
 
-elif [[ "$MODE" == "linux-amd64" ]]; then
+linux-amd64)
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
     file "$FILENAME"
+    ;;
 
-elif [[ "$MODE" == "linux-arm64" ]]; then
+linux-arm64)
     CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
     file "$FILENAME"
+    ;;
 
-elif [[ "$MODE" == "linux-arm-v7" ]]; then
+linux-arm-v7)
     CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
     file "$FILENAME"
+    ;;
 
-else
-    CGO_ENABLED=0 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
-    "./$FILENAME" --version
-fi
-
-if [[ "$MODE" == "integration" ]]; then
-    "./$FILENAME" wrap-yourself && go test -v "${PKGS[@]}"
-fi
+*)
+    echo "Unknown MODE: $MODE" >&2
+    exit 1
+    ;;
+esac
