559d8b104dd86fcb5aa2624ff4aafdca0276a4d1
benjamin <benjamin@stiffcoyote-e9263.denbi.prod>
2026-05-12 12:23:24 +0000

    format: gofmt

--- auth/auth.go
+++ auth/auth.go
@@ -32,7 +32,7 @@ func (a *authenticationInfo) UnmarshalString(s string) error {
 	a.Username = u.User.Username()
 	a.Password, _ = u.User.Password()
 
-	if u.Host + u.Path == "index.docker.io/v1/" {
+	if u.Host+u.Path == "index.docker.io/v1/" {
 		a.ServerAddress = u.Host + u.Path
 	} else {
 		a.ServerAddress = u.Host
