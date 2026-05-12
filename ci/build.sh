633082ee20bf81d39f1d026db716a2d3a7d3b09b
benjamin <benjamin@stiffcoyote-e9263.denbi.prod>
2026-05-12 12:22:18 +0000

    format: shellformat

--- ci/build.sh
+++ ci/build.sh
@@ -6,30 +6,30 @@ VERSION="$(git describe --tags --always)"
 LDFLAGS="-s -w -X github.com/involucro/involucro/app.version=$VERSION"
 
 if [[ "$MODE" == "unit" ]]; then
-  go test -v -short "${PKGS[@]}"
+    go test -v -short "${PKGS[@]}"
 fi
 
 if [[ "$MODE" == "windows-build" ]]; then
-  go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-  "$GOPATH"/bin/goversioninfo "-product-version=$VERSION"
+    go get -u github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+    "$GOPATH"/bin/goversioninfo "-product-version=$VERSION"
 
-  CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o involucro.exe -ldflags "$LDFLAGS" ./cmd/involucro
-  CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o involucro32.exe -ldflags "$LDFLAGS" ./cmd/involucro
-  file involucro.exe involucro32.exe
+    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o involucro.exe -ldflags "$LDFLAGS" ./cmd/involucro
+    CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o involucro32.exe -ldflags "$LDFLAGS" ./cmd/involucro
+    file involucro.exe involucro32.exe
 
 elif [[ "$MODE" == "cross-arm64" ]]; then
-  CGO_ENABLED=0 GOARCH=arm64 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
-  file "$FILENAME"
+    CGO_ENABLED=0 GOARCH=arm64 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
+    file "$FILENAME"
 
 elif [[ "$MODE" == "cross-arm" ]]; then
-  CGO_ENABLED=0 GOARCH=arm go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
-  file "$FILENAME"
+    CGO_ENABLED=0 GOARCH=arm go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
+    file "$FILENAME"
 
 else
-  CGO_ENABLED=0 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
-  "./$FILENAME" --version
+    CGO_ENABLED=0 go build -o "$FILENAME" -ldflags "$LDFLAGS" ./cmd/involucro
+    "./$FILENAME" --version
 fi
 
 if [[ "$MODE" == "integration" ]]; then
-  "./$FILENAME" wrap-yourself && go test -v "${PKGS[@]}"
+    "./$FILENAME" wrap-yourself && go test -v "${PKGS[@]}"
 fi
--- justfile
+++ justfile
@@ -5,7 +5,7 @@ default:
 
 format:
     gofmt -w $(find . -path ./vendor -prune -o -name '*.go' -print)
-    shfmt -w ci/*.sh
+    shfmt -i 4 -w ci/*.sh
     stylua $(find . -path ./vendor -prune -o -name '*.lua' -print)
     prettier --write .github/workflows/*.yml appveyor.yml
     just --fmt --justfile justfile
