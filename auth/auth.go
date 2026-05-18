4c66b34871acb23fd087a1016fa430e46693ddaa
benjamin <benjamin@stiffcoyote-e9263.denbi.prod>
2026-05-18 09:47:08 +0000

    refactor: replace custom home dir function with standard library
    project is older than the os home dir function

--- auth/auth.go
+++ auth/auth.go
@@ -9,10 +9,9 @@ import (
 	"net/url"
 	"os"
 	"path"
-	"runtime"
 	"strings"
 
-	"github.com/fsouza/go-dockerclient"
+	docker "github.com/fsouza/go-dockerclient"
 )
 
 const ENV_NAME = "INVOLUCRO_AUTH"
@@ -70,7 +69,11 @@ func getAllFrom(r io.Reader) ([]authenticationInfo, error) {
 // However, if an error other than file not found occurs, this error will be
 // returned and the value of the other values is undefined.
 func ForServer(server string) (docker.AuthConfiguration, bool, error) {
-	return forServerWithFile(server, path.Join(userHomeDir(), ".involucro"))
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return docker.AuthConfiguration{}, false, err
+	}
+	return forServerWithFile(server, path.Join(home, ".involucro"))
 }
 
 func forServerWithFile(server string, filename string) (docker.AuthConfiguration, bool, error) {
@@ -127,15 +130,4 @@ func forServerInFile(server string, file io.Reader) (docker.AuthConfiguration, b
 	}
 
 	return docker.AuthConfiguration{}, false, nil
-}
-
-func userHomeDir() string {
-	if runtime.GOOS == "windows" {
-		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-		if home == "" {
-			home = os.Getenv("USERPROFILE")
-		}
-		return home
-	}
-	return os.Getenv("HOME")
 }
